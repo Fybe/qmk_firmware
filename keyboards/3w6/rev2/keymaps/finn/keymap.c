e78a05cd1f6e7022ddaed21ac5dd7a880e1d3153
Finn Bender <mailobender77x@gmail.com>
2025-05-01 13:23:33 +0200

    Layer switching on other key press

--- keyboards/3w6/rev2/keymaps/finn/keymap.c
+++ keyboards/3w6/rev2/keymaps/finn/keymap.c
@@ -99,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_NUM_FN] = LAYOUT(
         QK_BOOT,   KC_F9,   KC_F8,   KC_F7,  KC_F12,         _______,    KC_7,    KC_8,    KC_9, _______,
-        ______,   KC_F6,   KC_F5,   KC_F4,  KC_F11,         _______,    KC_4,    KC_5,    KC_6, KC_DOT,
+        _______,   KC_F6,   KC_F5,   KC_F4,  KC_F11,         _______,    KC_4,    KC_5,    KC_6, KC_DOT,
         _______,   KC_F3,   KC_F2,   KC_F1,  KC_F10,         _______,    KC_1,    KC_2,    KC_3, _______,
                              _______, _______, _______, _______, KC_0, _______
     ),
@@ -454,8 +454,15 @@ void adaptive_scan(uint16_t time) {
     del_mods(MOD_MASK_SHIFT);                                           \
     tap_code(kc1)
 
-
-uint16_t last_keypress = 0;
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case TT(_NUM_FN):
+    case TT(_NAV):
+            return true;
+        default:
+            return false;
+    }
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!adaptive_fire(keycode, record)) return false;
